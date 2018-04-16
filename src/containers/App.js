41de5c918e1756f2017c7794e32e98c2ffe695b8
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-16 18:14:03 -0500

    All dependencies installed

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    
+  };
+  render() {
+    return (
+      <div>Hello There</div>
+    );
+  }
+}
+export default App;
