114fe29c829604f8d015d8cf658b412fe09144b3
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-16 23:56:28 -0500

    Add answer to store

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,5 +1,7 @@
 import { RECEIVE_USERS } from '../actions/users'
 import { ADD_POLL } from '../actions/polls'
+import { ADD_ANSWER } from '../actions/answers'
+
 
 export default function users(state = {}, action) {
   switch(action.type) {
@@ -16,6 +18,15 @@ export default function users(state = {}, action) {
         [author]: {
           ...state[author],
           polls: state[author].polls.concat([id])
+        }
+      }
+    case ADD_ANSWER:
+      const user = state[action.authedUser]
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...user,
+          answers: user.answers.concat([action.id])
         }
       }
     default:
