1d5ca64a534af07812f0d3fb468e90ec601037d2
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-16 22:10:15 -0500

    Add poll UI

---
+++ src/containers/AddPoll.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+
+class AddPoll extends Component {
+  state = {
+    question: '',
+    a: '',
+    b: '',
+    c: '',
+    d: ''
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    // Redirect to /
+  }
+
+  handleInputChange = (e) => {
+    const { value, name } = e.target
+
+    this.setState(() => ({
+      [name]: value
+    }))
+  }
+
+  isDisabled = () => {
+    const { question, a, b, c, d } = this.state
+
+    return question === ''
+      || a === ''
+      || b === ''
+      || c === ''
+      || d === ''
+  }
+
+  render() {
+    const { question, a, b, c, d } = this.state
+    return (
+      <form className="add-form" onSubmit={this.handleSubmit}>
+        <h3 style={{ marginBottom: 5 }}>What is your question?</h3>
+        <input
+          value={question}
+          onChange={this.handleInputChange}
+          name='question'
+          className="input"
+          type="text"
+        />
+        <h3>What are the options?</h3>
+        <label className="label" htmlFor="a">A.</label>
+        <input
+          value={a}
+          onChange={this.handleInputChange}
+          name="a"
+          className="input"
+          type="text"
+          id="a"
+        />
+        <label className="label" htmlFor="b">B.</label>
+        <input
+          value={b}
+          onChange={this.handleInputChange}
+          name="b"
+          className="input"
+          type="text"
+          id="b"
+        />
+        <label className="label" htmlFor="c">C.</label>
+        <input
+          value={c}
+          onChange={this.handleInputChange}
+          name="c"
+          className="input"
+          type="text"
+          id="c"
+        />
+        <label className="label" htmlFor="d">D.</label>
+        <input
+          value={d}
+          onChange={this.handleInputChange}
+          name="d"
+          className="input"
+          type="text"
+          id="d"
+        />
+        <button className="btn" type="submit" disabled={this.isDisabled()}>Submit</button>
+      </form>
+    )
+  }
+}
+export default AddPoll
--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,7 @@ import Dashboard from './Dashboard'
 import LoadingBar from 'react-redux-loading'
 
 import Leaderboard from './Leaderboard'
+import AddPoll from './AddPoll'
 
 class App extends Component {
   componentDidMount() {
@@ -17,7 +18,7 @@ class App extends Component {
         <LoadingBar />
         {this.props.loading === true
           ? <div>Loading...</div>
-          : <Leaderboard />
+          : <AddPoll />
         }
       </div>
     )
