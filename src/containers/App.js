b3a81bab4192e50bd0d0bac49a5a9b5bacf8830c
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-16 19:48:15 -0500

    Getting app data

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import { handleInitialData } from '../actions/shared'
+import { connect } from 'react-redux'
 
 class App extends Component {
   state = {
 
   }
+
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+
   render() {
     return (
       <div>Starter Code</div>
     )
   }
 }
-export default App
+export default connect()(App)
